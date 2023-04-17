ef21a0ad15fdfda5bd30d6a9b6767ae28667454d
MansourWolou <Wolou0444@gmail.com>
2023-04-17 12:39:24 +0200

    bdd connexion and store table created

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Store } from './store/entity/store.entity';
 
+/**
+ * !FIX BDD CONNEXION FOR DEPLOY
+ * !la connexion avec une bdd externe marche pas : avec celle de georgio blem de permission et celle de 
+ * ! render je sais mm plus .
+ * *Pour le moment il faut utiliser une bdd local
+ */
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'app_user',
+      password: 'secretpassword',
+      database: 'app_db',
+      entities: [Store],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
