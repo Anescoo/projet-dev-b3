6cf33192f1823f7ff1e54ed08d8e8b68200fff72
MansourWolou <Wolou0444@gmail.com>
2023-04-25 08:57:12 +0200

    update

--- back/src/products/products.module.ts
+++ back/src/products/products.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { ProducService } from './product.service';
-import { Product } from './entity/product.entity';
 @Module({
   controllers: [ProductController],
 })
