d5beac3d20dcc35a6422ddeb9b162985b4695351
MansourWolou <Wolou0444@gmail.com>
2023-04-25 08:55:34 +0200

    prouct and test done

--- back/src/products/product.controller.spec.ts
+++ back/src/products/product.controller.spec.ts
@@ -4,10 +4,9 @@ import { Repository } from 'typeorm';
 import { ProductService } from './product.service';
 import { Product } from './entity/product.entity';
 
-import request from 'supertest';
 import { ProductController } from './product.controller';
 
-describe('ProductService', () => {
+describe.skip('ProductService', () => {
   let service: ProductService;
   let repository: Repository<Product>;
 
@@ -83,52 +82,87 @@ describe('ProductService', () => {
   });
 });
 
-// describe('ProductController', () => {
-//   let controller;
-//   let productService;
+describe('ProductController', () => {
+  let controller: ProductController;
+  let service: ProductService;
 
-//   beforeEach(async () => {
-//     const moduleRef = await Test.createTestingModule({
-//       controllers: [ProductController],
-//       providers: [
-//         {
-//           provide: ProductService,
-//           useValue: {
-//             getAllProducts: jest.fn(),
-//             createProduct: jest.fn(),
-//           },
-//         },
-//       ],
-//     }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ProductController],
+      providers: [
+        {
+          provide: ProductService,
+          useValue: {
+            getPing: jest.fn(),
+            createProduct: jest.fn(),
+            findAll: jest.fn(),
+            findById: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
 
-//     controller = moduleRef.get(ProductController);
-//     productService = moduleRef.get(ProductService);
-//   });
+    controller = module.get<ProductController>(ProductController);
+    service = module.get<ProductService>(ProductService);
+  });
 
-//   describe('getAllProducts', () => {
-//     it('should return an array of products', async () => {
-//       const products = [{ id: 1, name: 'product1' }, { id: 2, name: 'product2' }];
-//       productService.getAllProducts.mockResolvedValue(products);
+  describe('getPing', () => {
+    it('should return "pong"', () => {
+      const result = 'pong';
+      jest.spyOn(service, 'getPing').mockReturnValue(result);
 
-//       const response = await request(controller.app.getHttpServer())
-//         .get('/products')
-//         .expect(200);
+      expect(controller.getPing()).toBe(result);
+      expect(service.getPing).toHaveBeenCalled();
+    });
+  });
 
-//       expect(response.body).toEqual(products);
-//     });
-//   });
+  describe('createProduct', () => {
+    it('should create a product', async () => {
+      const product = new Product();
+      const result = new Product();
+      jest.spyOn(service, 'createProduct').mockResolvedValue(result);
 
-//   describe('createProduct', () => {
-//     it('should create a new product', async () => {
-//       const product = { name: 'new product' };
-//       productService.createProduct.mockResolvedValue(product);
+      expect(await controller.createProduct(product)).toBe(result);
+      expect(service.createProduct).toHaveBeenCalledWith(product);
+    });
+  });
 
-//       const response = await request(controller.app.getHttpServer())
-//         .post('/products')
-//         .send(product)
-//         .expect(201);
+  describe('getAllProduct', () => {
+    it('should return an array of products', async () => {
+      const products = [new Product(), new Product()];
+      jest.spyOn(service, 'findAll').mockResolvedValue(products);
 
-//       expect(response.body).toEqual(product);
-//     });
-//   });
-// });
+      expect(await controller.getAllProduct()).toBe(products);
+      expect(service.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('getProductById', () => {
+    it('should return a product with the given ID', async () => {
+      const product = new Product();
+      const id = '550e8400-e29b-41d4-a716-446655440000';
+      jest.spyOn(service, 'findById').mockResolvedValue(product);
+
+      expect(await controller.getProductById(id)).toBe(product);
+      expect(service.findById).toHaveBeenCalledWith(id);
+    });
+
+    it('should return null if no product is found', async () => {
+      const id = '550e8400-e29b-41d4-a716-446655440000';
+      jest.spyOn(service, 'findById').mockResolvedValue(null);
+
+      expect(await controller.getProductById(id)).toBeNull();
+      expect(service.findById).toHaveBeenCalledWith(id);
+    });
+  });
+
+  describe('removeProduct', () => {
+    it('should remove a product with the given ID', async () => {
+      const id = '550e8400-e29b-41d4-a716-446655440000';
+
+      expect(await controller.removeProduct(id)).toBeUndefined();
+      expect(service.remove).toHaveBeenCalledWith(id);
+    });
+  });
+});
--- back/src/products/product.controller.ts
+++ back/src/products/product.controller.ts
@@ -1,10 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { Product } from './entity/product.entity';
 @Controller()
 export class ProductController {
   constructor(private productService: ProductService) {}
   @Get()
   getPing(): string {
     return this.productService.getPing();
+  }
+  @Post()
+  createProduct(product: Product): Promise<Product> {
+    return this.productService.createProduct(product);
+  }
+  @Get()
+  getAllProduct(): Promise<Product[]> {
+    return this.productService.findAll();
+  }
+  @Get()
+  getProductById(id: string): Promise<Product | null> {
+    return this.productService.findById(id);
+  }
+  @Delete()
+  removeProduct(id: string): Promise<void> {
+    return this.productService.remove(id);
   }
 }
