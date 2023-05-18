84dd1a2d57f00a0469a93f1959f3ad398eee0ab8
MansourWolou <Wolou0444@gmail.com>
2023-05-18 13:53:01 +0200

    page structuré

--- front/lib/features/products/produst_page.dart
+++ front/lib/features/products/produst_page.dart
@@ -7,6 +7,29 @@ class Product extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.orange,);
+    return Column(
+      children: [
+        Container(color: Colors.purple,height: 100,),
+        Expanded(
+          child: Container(
+            color: Color.fromARGB(255, 240, 237, 237),
+            child:  GridView.count(
+                // Create a grid with 2 columns. If you change the scrollDirection to
+                // horizontal, this produces 2 rows.
+                crossAxisCount: 2,
+                // Generate 100 widgets that display their index in the List.
+                children: List.generate(50, (index) {
+                  return Center(
+                    child: Text(
+                      'Item $index',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  );
+                }),
+              ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- front/lib/main.dart
+++ front/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,//!FIX
       title: 'Shop App',
       theme: ThemeData(
         // This is the theme of your application.
@@ -25,7 +26,8 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue).copyWith(background: Colors.grey),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+            .copyWith(background: Colors.grey),
       ),
       home: const Authentication(),
       // home: const MyHomePage(title: 'Flutter Shop App'),
@@ -117,4 +119,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
