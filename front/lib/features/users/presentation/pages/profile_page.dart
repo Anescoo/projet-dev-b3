7b5d693d08c2d47e883c4acf822da799ea7d7a23
MansourWolou <Wolou0444@gmail.com>
2023-06-11 12:59:59 +0200

    profile page  started

--- front/lib/features/users/presentation/pages/profile_page.dart
+++ front/lib/features/users/presentation/pages/profile_page.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:front/features/users/presentation/components/profile_option.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green,);
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            color: const Color.fromARGB(92, 7, 160, 207),
+            height: 120,
+          ),
+          const Text(
+            "Profile",
+            style: TextStyle(color: Colors.grey, fontSize: 20),
+          ),
+          ProfileOption(
+            icon: Icons.account_circle,
+            option: 'Profile',
+            onPressed: () {},
+          ),
+          ProfileOption(
+            icon: Icons.manage_history,
+            option: 'Commandes',
+            onPressed: () {},
+          ),
+          ProfileOption(
+            icon: Icons.settings,
+            option: 'Paramètre',
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
+
